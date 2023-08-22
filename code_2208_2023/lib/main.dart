7f3f4ba3813d899c7cf75aa2d8579932cd9a585a
Larry Lo <larry@real.co>
2023-08-22 21:07:49 +0800

    feat: updated 16 paddings

--- code_2208_2023/lib/main.dart
+++ code_2208_2023/lib/main.dart
@@ -40,34 +40,36 @@ class MyHomePage extends StatelessWidget {
                 showModalBottomSheet<void>(
                   context: context,
                   builder: (BuildContext context) {
-                    return DraggableScrollableSheet(
-                      expand: false,
-                      builder: (_, ScrollController scrollController) {
-                        return Column(
-                          children: [
-                            Expanded(
-                              child: ListView.builder(
-                                controller: scrollController,
-                                itemCount: 250,
-                                itemBuilder: (_, index) {
-                                  return ListTile(
-                                    title: Text("Item: $index"),
-                                  );
-                                },
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Handle button press
-                                // exit bottom scroll
-                                Navigator.pop(context);
-                              },
-                              child: Text('Bottom Button'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
+                    return Padding(
+                        padding: EdgeInsets.all(16.0), // Add padding of 16dp
+                        child: DraggableScrollableSheet(
+                          expand: false,
+                          builder: (_, ScrollController scrollController) {
+                            return Column(
+                              children: [
+                                Expanded(
+                                  child: ListView.builder(
+                                    controller: scrollController,
+                                    itemCount: 250,
+                                    itemBuilder: (_, index) {
+                                      return ListTile(
+                                        title: Text("Item: $index"),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Handle button press
+                                    // exit bottom scroll
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text('Bottom Button'),
+                                ),
+                              ],
+                            );
+                          },
+                        ));
                   },
                 );
               },
